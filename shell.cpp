2d892c74b86909daa541218c09b7c586ff0ff87e
bvillarroelr <benjamin.villarroel3@gmail.com>
2024-09-03 14:30:27 -0400

    feat: colores incluidos para futuros usos, el prompt ahora es verde

--- shell.cpp
+++ shell.cpp
@@ -3,6 +3,24 @@
 #include <unistd.h>
 #include <pwd.h>
 #include <sys/utsname.h>
+// Colores para probar con prompt, excepciones, mensajes, etc
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
 
 const int num_commands = 3;
 const std::string commands[num_commands] = {"ls", "cd", "wc"};
@@ -17,7 +35,7 @@ int main(){
         std::string command;
         char hostname[1024];
         gethostname(hostname, sizeof(hostname));
-        std::cout << username << "@" << hostname << ":" << dir << "$ ";
+        std::cout <<  GREEN << username << "@" << hostname << ":" << dir << "$ " << RESET;
         std::getline(std::cin, command);
 
         int command_large = command.size();
