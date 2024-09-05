f6f8a9b422c10fc8809c07ec70cdde735618f3a7
agustin <llms.agustin@gmail.com>
2024-09-05 10:02:24 -0400

    "commit del ultimo pull"

---
+++ remember.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <unistd.h>
+#include <pwd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <vector>
+#include <cstring>
+#include <time.h>
+// numero de comandos 
+const int n_cmd = 3;
+const std::string cmds[n_cmd] = {"set", "recordatrio", "Tarea"};
+const std::string cmds_args[n_cmd] = {{},{},{}};
+int main(){
+        
+}
